6f1917d5095c7135b7c8859b389c7dbcc6e09a29
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-05-29 18:13:42 +0200

    add final files

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -5,13 +5,16 @@ import UsersList from "./components/Users/UsersList/UsersList";
 function App() {
   const [usersList, setUsersList] = useState([]);
   const addUserHandler = (userAge, userName) => {
-    setUsersList((prevUsersList)=>{
-      return [...prevUsersList, {name: userName, age: userAge, id: Math.random()}];
-    })
-  }
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random() },
+      ];
+    });
+  };
   return (
     <div>
-      <AddUser onAddUser={addUserHandler}/>
+      <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
     </div>
   );
