53a9d46c542899ec6bf33c9c93f71ac5fcce6b9a
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-06-17 19:20:36 +0200

    add section before pushing to github

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [showP, setShowP] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler = () => {
-    setShowP((prev) => !prev);
-  };
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showP && <p>This is new</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
     </div>
   );
