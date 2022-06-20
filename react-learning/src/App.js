cc929bf8da791c102e407cf4bb3549c3dcf799f7
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-06-20 17:01:01 +0200

    add own Form

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import BasicForm from './components/BasicForm';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
   return (
-    <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
-    </React.Fragment>
+    <div className="app">
+      <BasicForm />
+    </div>
   );
 }
 
