c0543295651f6129da2e0fc0588b259aa556168d
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-06-20 17:33:09 +0200

    add lesson summary

--- react-learning/src/hooks/useInput.js
+++ react-learning/src/hooks/useInput.js
@@ -1,25 +1,50 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const actionTypes = {
+  input: "INPUT",
+  blur: "BLUR",
+  reset: "RESET",
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === actionTypes.input) {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === actionTypes.blur) {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === actionTypes.reset) {
+    return { value: "", isTouched: false };
+  }
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatchInputState] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (e) => {
-    setEnteredValue(e.target.value);
+    dispatchInputState({ type: actionTypes.input, value: e.target.value });
   };
   const inputBlurHandler = (e) => {
-    setIsTouched(true);
+    dispatchInputState({ type: actionTypes.blur });
   };
-  const reset = ()=>{
-    setEnteredValue('');
-    setIsTouched(false)
-  }
+  const reset = () => {
+    dispatchInputState({ type: actionTypes.reset });
+  };
 
   return {
-    value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
