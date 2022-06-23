9978df32dcc0e0b0793d815db97df0ef4179aa63
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-06-23 16:03:46 +0200

    add final files

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -3,8 +3,8 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 
 let isInitial = true;
 
@@ -14,47 +14,18 @@ function App() {
   const notificationStatus = useSelector((state) => state.ui.notification);
 
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
-    if(isInitial){
+    if (isInitial) {
       isInitial = false;
       return;
     }
-    dispatch(
-      uiActions.setNotification({
-        status: "pending",
-        title: "sending",
-        message: "sending cart data",
-      })
-    );
-    const sendCartData = async () => {
-      const response = await fetch(
-        "https://react-http-c8b0d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cartState),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("something went wrong");
-      }
-      dispatch(
-        uiActions.setNotification({
-          status: "success",
-          title: "success!",
-          message: "succesfully sent cart data",
-        })
-      );
-    };
-    sendCartData().catch((error) =>
-      dispatch(
-        uiActions.setNotification({
-          status: "error",
-          title: "error!",
-          message: error.message,
-        })
-      )
-    );
+    if (cartState.changed) {
+      dispatch(sendCartData(cartState));
+    }
   }, [cartState, dispatch]);
 
   return (
--- redux-learning/src/store/cart-slice.js
+++ redux-learning/src/store/cart-slice.js
@@ -3,18 +3,24 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   items: [],
   totalQuantity: 0,
+  changed: false,
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       // USTAWIAMY NOWY ITEM
       const newItem = action.payload;
       //   SPRAWDZAMY CZY JUŻ ISTNIEJE TAKI ITEM
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       //   JEŻELI NIE, TO PUSHUJEMY NOWY ITEM DO OBECNEJ TABLICY
       if (!existingItem) {
         state.items.push({
@@ -35,6 +41,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
