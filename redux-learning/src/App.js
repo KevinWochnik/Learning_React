d269c79568ed78ff8980882da91d595aac9f4fde
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-06-25 16:24:23 +0200

    add initial files

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -1,47 +1,13 @@
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import Notification from "./components/UI/Notification";
-import { fetchCartData, sendCartData } from "./store/cart-actions";
-
-let isInitial = true;
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cartState = useSelector((state) => state.cart);
-  const notificationStatus = useSelector((state) => state.ui.notification);
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchCartData());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (isInitial) {
-      isInitial = false;
-      return;
-    }
-    if (cartState.changed) {
-      dispatch(sendCartData(cartState));
-    }
-  }, [cartState, dispatch]);
-
   return (
-    <>
-      {notificationStatus && (
-        <Notification
-          status={notificationStatus.status}
-          title={notificationStatus.title}
-          message={notificationStatus.message}
-        />
-      )}
-      <Layout>
-        {showCart && <Cart />}
-        <Products />
-      </Layout>
-    </>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
