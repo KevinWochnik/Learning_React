d7ca063f76cef687428acd9f1ab3093c7799fc5a
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-06-25 20:01:36 +0200

    add files before starting project

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -3,12 +3,7 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
-  );
+  return ();
 }
 
 export default App;
---
+++ redux-learning/src/assets/styles/GlobalStyle.js
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    white: "#ffffff",
+    lightGrey: "#D9D9D9",
+    darkGrey: "#555555",
+    lightGreen: "#7FFBA2",
+  },
+  fontSize: {
+    s: "12",
+    m: "16",
+    l: "18",
+    xl: "24",
+    xxl: "30",
+  },
+};
