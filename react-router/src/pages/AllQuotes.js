81973ce42e84fc54b0df1a3e6e676b5d2f17994a
Kevin Wochnik <wochnik.kevin@yahoo.com>
2022-07-02 11:35:10 +0200

    add initial files

---
+++ react-router/src/pages/AllQuotes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllQuotes = ()=>(
+<h1>All Quotes</h1>
+);
+
+export default AllQuotes;
---
+++ react-router/src/pages/NewQuote.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewQuote = ()=>(
+<h1>NewQuote</h1>
+);
+
+export default NewQuote;
